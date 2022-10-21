f9f66421ad953f1c321e7ec33ad260d361b09f43
Aura <aura.arbelaez@upb.edu.co>
2022-10-20 21:45:28 -0500

    firebase deploy

--- js/nota.js
+++ js/nota.js
@@ -1,15 +1,12 @@
+import {v4 as uuidv4} from 'uuid';
 class Nota{
     constructor( titulo, contenido, tags){
-        this.id= this.asignacionID;
+        this.id= uuidv4();
         this.fechaCreacion= String.valueOf(new Date.now());
         this.horaCreacion= String.valueOf(this.fechaCreacion.getHours()) +String.valueOf(this.fechaCreacion.getMinutes());
         this.titulo= titulo;
         this.contenido= contenido;
         this.tags= tags;
-    }
-    asignacionID(){
-        id= Math.random()*10000;
-        if()
     }
 }
 
