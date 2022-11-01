daac731d2adc8f3de4f1fafdeedd881b01cbc3c9
mtohernandez <m4teoh3rnandez@outlook.com>
2022-11-01 15:07:57 -0500

    Update mobile responsiveness

--- js/home.js
+++ js/home.js
@@ -4,4 +4,21 @@ const gradient = new Gradient();
 
 gradient.initGradient('#gradient-canvas');
 
+const openNav = document.getElementById("nav__toggle"),
+  closeNav = document.getElementById("nav__close"),
+  navBar = document.getElementById("nav-menu"),
+  overlay = document.querySelector(".overlay");
 
+if (openNav) {
+  openNav.addEventListener("click", function () {
+    navBar.classList.add("show-menu");
+    overlay.classList.remove('hidden');
+  });
+}
+
+if (closeNav) {
+  closeNav.addEventListener("click", function () {
+    navBar.classList.remove("show-menu");
+    overlay.classList.add('hidden');
+  });
+}
