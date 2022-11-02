a2d27a87bec3295e21f2697fd46482610bfa4d27
mtohernandez <m4teoh3rnandez@outlook.com>
2022-11-02 18:06:34 -0500

    Update notes

--- js/notes.js
+++ js/notes.js
@@ -2,16 +2,16 @@
 
 window.onload = () => {
   const white = "#fff";
-  const nav = document.querySelector('.nav');
+  const nav = document.querySelector(".nav");
   nav.style.backgroundColor = white;
+};
 
+const elem = document.querySelector(".notes-section__container");
 
-  const elem = document.querySelector('.notes-section__container');
+const masonry = new Masonry(elem, {
+  itemSelector: ".note",
+  gutter: 15,
+  percentPosition: true,
+  trasitionDuration: "0.15s",
+});
 
-  const masonry = new Masonry(elem, {
-    itemSelector: ".note",
-    gutter: 15,
-    percentPosition: true,
-    trasitionDuration: '0.15s'
-  });
-}
