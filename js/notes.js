c17f8581a32e6813b959009249448c22ba9d5ca7
mtohernandez <m4teoh3rnandez@outlook.com>
2022-11-02 15:47:22 -0500

    Update notes design and layout

--- js/notes.js
+++ js/notes.js
@@ -1,2 +1,12 @@
 "use strict";
 
+window.onload = () => {
+  const elem = document.querySelector('.notes-section__container');
+
+  const masonry = new Masonry(elem, {
+    itemSelector: ".note",
+    gutter: 15,
+    percentPosition: true,
+    trasitionDuration: '0.15s'
+  });
+}
