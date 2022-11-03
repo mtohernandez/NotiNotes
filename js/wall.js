cda72d4d139905195ce4c7fa703b10bbd39b3ad7
mtohernandez <m4teoh3rnandez@outlook.com>
2022-11-03 18:34:21 -0500

    Add the wall main functionalities

--- js/wall.js
+++ js/wall.js
@@ -3,3 +3,28 @@ window.onload = () => {
   const nav = document.querySelector(".nav");
   nav.style.backgroundColor = white;
 };
+
+
+const items = document.querySelectorAll('.items-item'),
+wall = document.querySelector('.wall-container');
+
+let wallElements = document.querySelectorAll('.wall-element');
+
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    const html = `<div class="wall-element"> 
+                  <${item.textContent.trim()} contenteditable="true">Add Content Here...
+                  <div contenteditable="false" class="tooltip">-</div></${item.textContent.trim()}>
+                  </div>`;
+    
+    wall.insertAdjacentHTML('beforeend', html);
+    wallElements = document.querySelectorAll('.wall-element')
+    wallElements.forEach(elem => {
+      
+      elem.querySelector('.tooltip').addEventListener('click', () => {
+        elem.remove();
+      })
+    })
+  })
+})
+
