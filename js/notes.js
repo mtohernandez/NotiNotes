0a5f33a56f1c0405d971555871019584574b74d1
mtohernandez <m4teoh3rnandez@outlook.com>
2022-11-03 14:53:55 -0500

    Fix centered notes

--- js/notes.js
+++ js/notes.js
@@ -11,7 +11,7 @@ const elem = document.querySelector(".notes-section__container");
 const masonry = new Masonry(elem, {
   itemSelector: ".note",
   gutter: 15,
-  percentPosition: true,
+  isFitWidth: true, /* Life saver */
   trasitionDuration: "0.15s",
 });
 
