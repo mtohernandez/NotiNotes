a38a78d085846cc3d5b4f226d96dcb2ddd413742
mtohernandez <m4teoh3rnandez@outlook.com>
2022-11-04 11:11:55 -0500

    Add image feature

--- js/wall.js
+++ js/wall.js
@@ -1,30 +1,47 @@
+'use strict';
+
 window.onload = () => {
   const white = "#fff";
   const nav = document.querySelector(".nav");
   nav.style.backgroundColor = white;
 };
 
+const items = document.querySelectorAll(".items-item"),
+  wall = document.querySelector(".wall-container");
 
-const items = document.querySelectorAll('.items-item'),
-wall = document.querySelector('.wall-container');
+let wallElements = document.querySelectorAll(".wall-element");
 
-let wallElements = document.querySelectorAll('.wall-element');
+let max = 1000, min = 1;
+const randomImg = 'IMG' + Math.trunc(Math.random() * (max - min) + min);
 
-items.forEach(item => {
-  item.addEventListener('click', () => {
+function loadFile(event) {
+  const html = `<div class="wall-element" style='display: inline-block'>
+                <img src='${URL.createObjectURL(event.target.files[0])}'
+                style="width: 300px; height: 300px; object-fit: cover;"/>
+                <div contenteditable="false" class="tooltip">-</div>
+                </div>`;
+  wall.insertAdjacentHTML("beforeend", html)
+}
+
+
+
+items.forEach((item) => {
+  item.addEventListener("click", () => {
     const html = `<div class="wall-element"> 
-                  <${item.textContent.trim()} contenteditable="true">Add Content Here...
-                  <div contenteditable="false" class="tooltip">-</div></${item.textContent.trim()}>
+                    <${item.textContent.trim()}>
+                      <span class="textarea" role="textbox" contenteditable></span>
+                      <div contenteditable="false" class="tooltip">-</div>
+                    </${item.textContent.trim()}>
                   </div>`;
-    
-    wall.insertAdjacentHTML('beforeend', html);
-    wallElements = document.querySelectorAll('.wall-element')
-    wallElements.forEach(elem => {
-      
-      elem.querySelector('.tooltip').addEventListener('click', () => {
+
+    item.textContent.trim() !== 'img' ? wall.insertAdjacentHTML("beforeend", html) : "";
+    wallElements = document.querySelectorAll(".wall-element");
+    wallElements.forEach((elem) => {
+      elem.querySelector(".tooltip").addEventListener("click", () => {
         elem.remove();
-      })
-    })
-  })
-})
+      });
+    });
+  });
+});
+
 
