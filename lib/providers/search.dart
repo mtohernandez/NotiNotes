911b6b85c81b1a46b558106e0ec3488bd54d5f28
Mateo Hernandez <m4teoh3rnandez@outlook.com>
2026-04-05 22:30:38 -0500

    Add NoteFilter enum to search provider

--- lib/providers/search.dart
+++ lib/providers/search.dart
@@ -6,14 +6,24 @@ enum SearchType {
   searchingByTag,
 }
 
+/// Top-bar filter chips on the home screen.
+enum NoteFilter { all, reminders, checklists, images }
+
 class Search with ChangeNotifier {
   SearchType _isSearching = SearchType.notSearching;
   String _searchQuery = '';
   final Set<String> _searchTags = {};
+  NoteFilter _filter = NoteFilter.all;
 
   SearchType get isSearching => _isSearching;
   String get searchQuery => _searchQuery;
   Set<String> get searchTags => _searchTags;
+  NoteFilter get filter => _filter;
+
+  void setFilter(NoteFilter f) {
+    _filter = f;
+    notifyListeners();
+  }
 
   void activateSearchByTitle() {
     _isSearching = SearchType.searchingByTitle;
